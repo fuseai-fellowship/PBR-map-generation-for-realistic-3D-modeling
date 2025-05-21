48208b94c7e0f2d47f85d6d549c798452acb57ae
Kingyakstha <kingyakshrestha123@gmail.com>
2025-05-21 10:14:42 +0545

    small change for camera

--- majorFront/src/Components/sample.jsx
+++ majorFront/src/Components/sample.jsx
@@ -23,7 +23,7 @@ const Sample = () => {
 
   useEffect(()=>{
     setHasMultipleCameras(currentAsset.numberOfCamera)
-  },[])
+  },[currentAsset.numberOfCamera])
 
   const startCamera = () => {
     console.log("Multiple is ",hasMultipleCameras)
@@ -207,7 +207,7 @@ const Sample = () => {
               className="bg-white text-gray-700 px-6 py-2 rounded-full shadow-lg hover:bg-gray-100 transition-colors w-40 text-center"
             >
               Close
-              {hasMultipleCameras?'Hello':'Hi'}
+
             </motion.button>
 
             {hasMultipleCameras && (
--- majorFront/src/Pages/NewLandingPage.jsx
+++ majorFront/src/Pages/NewLandingPage.jsx
@@ -23,6 +23,7 @@ const NewLandingPage = () => {
         const devices = await navigator.mediaDevices.enumerateDevices();
         const videoInputs = devices.filter(device => device.kind === 'videoinput');
         console.log("Video Inputs are ", videoInputs)
+        alert("Not More")
         if (videoInputs.length > 1) {
           // setHasMultipleCameras(true);
           alert("More")
